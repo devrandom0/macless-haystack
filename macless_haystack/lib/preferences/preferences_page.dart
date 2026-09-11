e46aaeadb97990091c8becbdc052c4ef76daf43c
Sina Moghaddas <sina.moghaddas@gmail.com>
2026-09-11 23:22:27 +0330

    feat: add all-devices server-side archiving toggle

--- macless_haystack/lib/preferences/preferences_page.dart
+++ macless_haystack/lib/preferences/preferences_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:macless_haystack/accessory/accessory_model.dart';
+import 'package:macless_haystack/accessory/accessory_registry.dart';
+import 'package:macless_haystack/history/history_archive_service.dart';
 import 'package:macless_haystack/location/location_model.dart';
 import 'package:macless_haystack/preferences/user_preferences_model.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
@@ -15,6 +18,15 @@ class PreferencesPage extends StatefulWidget {
 }
 
 class _PreferencesPageState extends State<PreferencesPage> {
+  bool _archivingLoading = true;
+  bool _archivingAllEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadArchivingStatus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +42,7 @@ class _PreferencesPageState extends State<PreferencesPage> {
             getUserTile(),
             getPassTile(),
             getNumberofDaysTile(),
+            getArchiveAllTile(),
             ListTile(
               title: getAbout(),
             ),
@@ -130,6 +143,98 @@ class _PreferencesPageState extends State<PreferencesPage> {
       settingKey: fetchLocationOnStartupKey,
       defaultValue: true,
       title: 'Fetch locations on startup',
+    );
+  }
+
+  Set<String> _allKnownKeys(Iterable<Accessory> accessories) {
+    var keys = <String>{};
+    for (var accessory in accessories) {
+      keys.add(accessory.hashedPublicKey);
+      keys.addAll(accessory.additionalKeys);
+    }
+    return keys;
+  }
+
+  Future<void> _loadArchivingStatus() async {
+    try {
+      var url = Settings.getValue<String>(endpointUrl, defaultValue: 'http://localhost:6176')!;
+      var user = Settings.getValue<String>(endpointUser, defaultValue: '')!;
+      var pass = Settings.getValue<String>(endpointPass, defaultValue: '')!;
+      var accessories = Provider.of<AccessoryRegistry>(context, listen: false).accessories;
+      var allKeys = _allKnownKeys(accessories);
+
+      var devices = await HistoryArchiveService.getArchivedDevices(url, user, pass);
+      var enabledKeys = devices.where((d) => d.enabled).map((d) => d.hashedPublicKey).toSet();
+      var allEnabled = allKeys.isNotEmpty && allKeys.every(enabledKeys.contains);
+
+      if (mounted) {
+        setState(() {
+          _archivingAllEnabled = allEnabled;
+          _archivingLoading = false;
+        });
+      }
+    } catch (e) {
+      if (mounted) {
+        setState(() {
+          _archivingLoading = false;
+        });
+      }
+    }
+  }
+
+  Future<void> _setArchivingAll(bool enabled) async {
+    var previous = _archivingAllEnabled;
+    setState(() {
+      _archivingAllEnabled = enabled;
+    });
+    try {
+      var url = Settings.getValue<String>(endpointUrl, defaultValue: 'http://localhost:6176')!;
+      var user = Settings.getValue<String>(endpointUser, defaultValue: '')!;
+      var pass = Settings.getValue<String>(endpointPass, defaultValue: '')!;
+      var accessories = Provider.of<AccessoryRegistry>(context, listen: false).accessories;
+
+      List<HistoryDeviceEntry> devices = [];
+      for (var accessory in accessories) {
+        var additionalPrivateKeys = await accessory.getAdditionalPrivateKeys();
+        devices.add(HistoryDeviceEntry(
+          hashedPublicKey: accessory.hashedPublicKey,
+          privateKey: await accessory.getPrivateKey(),
+          name: accessory.name,
+          accessoryId: accessory.id,
+          enabled: enabled,
+        ));
+        for (var i = 0; i < accessory.additionalKeys.length; i++) {
+          devices.add(HistoryDeviceEntry(
+            hashedPublicKey: accessory.additionalKeys[i],
+            privateKey: additionalPrivateKeys[i],
+            name: '${accessory.name} (extra key)',
+            accessoryId: accessory.id,
+            enabled: enabled,
+          ));
+        }
+      }
+
+      if (devices.isNotEmpty) {
+        await HistoryArchiveService.setDevicesArchiving(url, user, pass, devices);
+      }
+    } catch (e) {
+      if (mounted) {
+        setState(() {
+          _archivingAllEnabled = previous;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Could not update server-side archiving: $e')),
+        );
+      }
+    }
+  }
+
+  getArchiveAllTile() {
+    return SwitchListTile(
+      value: _archivingAllEnabled,
+      title: const Text('Archive all devices on server'),
+      subtitle: _archivingLoading ? const Text('Loading status…') : null,
+      onChanged: _archivingLoading ? null : _setArchivingAll,
     );
   }
 }
